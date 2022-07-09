1a25e6d800651c89831967c66c0a4707963fae8b
IhorLyskov <99328897+IhorLyskov@users.noreply.github.com>
2022-07-09 15:17:17 +0300

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,13 @@
+const inputRef = document.querySelector("#validation-input");
+
+inputRef.addEventListener("blur", onInputTextBlur);
+
+function onInputTextBlur(event) {
+  if (Number(inputRef.dataset.length) === event.currentTarget.value.length) {
+    inputRef.classList.add("valid");
+    inputRef.classList.remove("invalid");
+  } else {
+    inputRef.classList.add("invalid");
+    inputRef.classList.remove("valid");
+  }
+}
