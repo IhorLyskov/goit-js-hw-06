943b54082c313aa30b9be1e9b472f54b3c3d7b4b
IhorLyskov <99328897+IhorLyskov@users.noreply.github.com>
2022-07-10 14:45:46 +0300

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,83 +1,46 @@
-const getUserNames = (users) => users.map((user) => user.name);
+const atTheOldToad = {
+  potions: [
+    { name: "Speed potion", price: 460 },
+    { name: "Dragon breath", price: 780 },
+    { name: "Stone skin", price: 520 },
+  ],
+  // Change code below this line
+  getPotions() {
+    return this.potions;
+  },
+  addPotion(potion) {
+    const potions = this.potions;
+    const { name: newName } = potion;
+    for (const { name } of potions) {
+      if (name === newName) {
+        return `Error! Potion ${newName} is already in your inventory!`;
+      }
+    }
+    potions.push(potion);
+    return `Potion ${newName} is added`;
+  },
+  removePotion(potionName) {
+    const potions = this.potions;
 
-const users = [
-  {
-    name: "Moore Hensley",
-    email: "moorehensley@indexia.com",
-    eyeColor: "blue",
-    friends: ["Sharron Pace"],
-    isActive: false,
-    balance: 2811,
-    skills: ["ipsum", "lorem"],
-    gender: "male",
-    age: 37,
+    for (let i = 0; i < potions.length; i += 1) {
+      const { name } = potions[i];
+      if (name === potionName) {
+        potions.splice(i, 1);
+        return `Potion ${potionName} is deleted`;
+      }
+    }
+    return `Error! Potion ${potionName} is not found`;
   },
-  {
-    name: "Sharlene Bush",
-    email: "sharlenebush@tubesys.com",
-    eyeColor: "blue",
-    friends: ["Briana Decker", "Sharron Pace"],
-    isActive: true,
-    balance: 3821,
-    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
-    gender: "female",
-    age: 34,
-  },
-  {
-    name: "Ross Vazquez",
-    email: "rossvazquez@xinware.com",
-    eyeColor: "green",
-    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-    isActive: false,
-    balance: 3793,
-    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
-    gender: "male",
-    age: 24,
-  },
-  {
-    name: "Elma Head",
-    email: "elmahead@omatom.com",
-    eyeColor: "green",
-    friends: ["Goldie Gentry", "Aisha Tran"],
-    isActive: true,
-    balance: 2278,
-    skills: ["adipisicing", "irure", "velit"],
-    gender: "female",
-    age: 21,
-  },
-  {
-    name: "Carey Barr",
-    email: "careybarr@nurali.com",
-    eyeColor: "blue",
-    friends: ["Jordan Sampson", "Eddie Strong"],
-    isActive: true,
-    balance: 3951,
-    skills: ["ex", "culpa", "nostrud"],
-    gender: "male",
-    age: 27,
-  },
-  {
-    name: "Blackburn Dotson",
-    email: "blackburndotson@furnigeer.com",
-    eyeColor: "brown",
-    friends: ["Jacklyn Lucas", "Linda Chapman"],
-    isActive: false,
-    balance: 1498,
-    skills: ["non", "amet", "ipsum"],
-    gender: "male",
-    age: 38,
-  },
-  {
-    name: "Sheree Anthony",
-    email: "shereeanthony@kog.com",
-    eyeColor: "brown",
-    friends: ["Goldie Gentry", "Briana Decker"],
-    isActive: true,
-    balance: 2764,
-    skills: ["lorem", "veniam", "culpa"],
-    gender: "female",
-    age: 39,
-  },
-];
 
-console.log(getUserNames(users));
+  updatePotionName(oldName, newName) {
+    for (let i = 0; i < this.potions.length; i += 1) {
+      const { name } = this.potions[i];
+      if (name === oldName) {
+        this.potions[i].name = newName;
+        return;
+      }
+    }
+  },
+  // Change code above this line
+};
+atTheOldToad.updatePotionName("Stone skin", "Invulnerability potion");
